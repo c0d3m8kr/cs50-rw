10a42d7bf8a8a99cf6363cf4e882e794160aaafc
nocz-desk <c0d3m8kr@gmail.com>
2022-02-16 22:38:10 -0500

    Added binarysearchtree

---
+++ binarysearchtree.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int number;
+    struct node *left;
+    struct node *right;
+}
+node;
+
+void free_tree(node *root);
+void print_tree(node *root);
+
+int main()
+{
+    
+
+    return 0;
+}
